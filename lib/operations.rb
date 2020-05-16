8105062a396a28238e369560201a102bbfcf0aff
PARDEEP KAUR <pkaur84@aol.com>
2020-05-16 01:22:26 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,17 @@
-def unsafe?(speed)
-  Speed < 40 || Speed > 60
-if Speed < 40 || Speed > 60
-    return true
-  else
-    return false
-  end
-end
+ef unsafe?(speed)
+
+  if speed > 60
+   true
+ elsif speed < 40
+   true
+ else
+   false
+ end
+end	
 
 
-def not_safe?(speed)
-	speed < 40 || speed > 60
+
+def not_safe?(speed)	def not_safe?(speed)
+		   speed > 60 || speed < 40 ? true : false
+ end	
 end
