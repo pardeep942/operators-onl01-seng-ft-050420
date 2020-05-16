23b6ed3320556a486d6007a326a9a9fdaa51e6e6
PARDEEP KAUR <pkaur84@aol.com>
2020-05-16 01:16:59 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,13 @@
 def unsafe?(speed)
-
+  Speed < 40 || Speed > 60
+if Speed < 40 || Speed > 60
+    return true
+  else
+    return false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60
 end
-	
-
-
